a18b410f6655997a27f042103561d98f90eed6ee
imustend <plochockitymon@gmail.com>
2022-12-12 16:31:48 +0100

    inital commit #2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './main/main.component';
+import { PlayersComponent } from './players/players.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent, pathMatch: 'full'},
+  { path: 'players', component: PlayersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { PlayersComponent } from './players/players.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    PlayersComponent
   ],
   imports: [
     BrowserModule,
